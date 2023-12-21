bdc642494d78b7a293dc9f795652e5de90293dae
rliu <984544334@qq.com>
2023-12-21 17:33:38 +0800

    remove verbose schema

--- internal/schema/download.go
+++
@@ -1,6 +0,0 @@
-package schema
-
-type DownloadReq struct {
-	FileLink string `json:"file_link" form:"file_link" binding:"required"`
-	FileType string `json:"file_type" form:"file_type" binding:"required"`
-}
